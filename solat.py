eddb39df92d2edd912a8cdacfba0676691acb8ab
Nabil <nabilalbany17@gmail.com>
2022-11-07 10:02:33 +0700

    tugaspython1

---
+++ solat.py
@@ -0,0 +1,20 @@
+# soal no 1
+print ("Hello World Python") 
+
+#soal no 2 
+TKJ = 23
+RPL = 10
+
+print (TKJ + RPL)
+print (TKJ - RPL)
+print (TKJ * RPL)
+print (TKJ / RPL)
+
+#Soal no 3
+print (int(TKJ / RPL))
+print (float(TKJ / RPL))
+
+#Soal no 4
+usia = 16
+kata = 'usiaku :'
+print (kata, usia)
